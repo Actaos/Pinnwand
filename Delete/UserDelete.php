71839aac63003a57ebd4b2f5042567842537297d
TillSchmidt <till74575@gmail.com>
2018-07-03 09:55:46 +0200

    Update UserDelete.php

--- Delete/UserDelete.php
+++ Delete/UserDelete.php
@@ -1,8 +1,12 @@
 <?php
-function delete_User($servername,$userId,$username,$password)
+function delete_User($userId,$username,$password)
 {
+ if($userId==null)
+ {
+  return "user id can not be null";
+ }
 try{
- $conn = new PDO("mysql:host=$servername;dbname=pinwanddb",$username,$password);
+ $conn = new PDO("mysql:host=localhost;dbname=pinwanddb",$username,$password);
  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
  $sql="DELETE FROM `benutzer`WHERE `b_id==".$userId."`";
 $conn->exec($sql);
