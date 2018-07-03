89d5f49e851112ab435dbe96cec919b4e939996a
TillSchmidt <till74575@gmail.com>
2018-07-03 10:07:52 +0200

    Update EntryCSVExport.php

--- Export/EntryCSVExport.php
+++ Export/EntryCSVExport.php
@@ -1,11 +1,11 @@
 <?php
- function csvExport_Entries($servername,$username,$password)
+ function csvExport_Entries($servername)
  {
       header('Content-Type: text/csv; charset=utf-8');  
       header('Content-Disposition: attachment; filename=entries.csv');  
       $output = fopen("php://output", "w");  
       fputcsv($output, array('id','Titel', 'Text', 'Autor','Autor'));  
-       $conn = new PDO("mysql:host=localhost;dbname=pinwanddb", $username, $password);
+       $conn = new PDO("mysql:host=localhost;dbname=pinwanddb",root);
        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
        $sql = "SELECT e_id,e_titel,e_text,b_vorname,b_nachname from eintraege,benutzer WHERE benuter.b_id==eintraege.b_id";  
        $result=$conn->exec($sql);
