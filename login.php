8bd8b9224679d90ff493b5e1efe1775c90c4eb27
Unknown <Georg.Popp2000@web.de>
2018-07-03 10:04:36 +0200

    Login page added
    + css/signin.css
    + login.php

---
+++ login.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
+
+    <title>Signin Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="./css/sigin.css" rel="stylesheet">
+</head>
+
+<body class="text-center">
+<form class="form-signin">
+    <img class="mb-4" src="https://cdn.icon-icons.com/icons2/877/PNG/512/inclinated-pinboard_icon-icons.com_68395.png"
+         alt="" width="72" height="72">
+    <h1 class="h3 mb-3 font-weight-normal">Einloggen</h1>
+    <label for="inputEmail" class="sr-only">Benutzername</label>
+    <input type="email" id="inputEmail" class="form-control" placeholder="Benutzername" required autofocus>
+    <label for="inputPassword" class="sr-only">Password</label>
+    <input type="password" id="inputPassword" class="form-control" placeholder="Passwort" required>
+    <div class="checkbox mb-3">
+        <label>
+            <input type="checkbox" value="remember-me"> Benutzername speichern
+        </label>
+    </div>
+    <button class="btn btn-lg btn-primary btn-block" type="submit">Einloggen</button>
+    <div>
+        <a href="index.php">Login überspringen</a>
+    </div>
+    <p class="mt-5 mb-3 text-muted">&copy; 2018</p>
+</form>
+</body>
+</html>
