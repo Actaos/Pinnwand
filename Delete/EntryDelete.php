398f1bd6a889bdb8d0a313c0966aeb08ee759d62
TillSchmidt <till74575@gmail.com>
2018-07-03 10:07:08 +0200

    Update EntryDelete.php

--- Delete/EntryDelete.php
+++ Delete/EntryDelete.php
@@ -1,12 +1,12 @@
 <?php
-function delete_Entry($id,$username,$password,)
+function delete_Entry($id)
 {
  if($id==null)
  {
   return "id can not be null"; 
  }
 try{
- $conn = new PDO("mysql:host=localhost;dbname=pinwanddb",$username,$password);
+ $conn = new PDO("mysql:host=localhost;dbname=pinwanddb",root);
  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
  $sql="DELETE FROM `eintraege`WHERE `e_id==".$id."`";
 $conn->exec($sql);
