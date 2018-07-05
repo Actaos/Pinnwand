6997eff57cb0a4cd51a22ff8df2c4455ccf3aaab
TillSchmidt <till74575@gmail.com>
2018-07-05 19:08:15 +0200

    commit: added TestData

--- overview/pinnwanddb.sql
+++ overview/pinnwanddb.sql
@@ -1,26 +1,26 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.4.1
--- http://www.phpmyadmin.net
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 03. Jul 2018 um 09:51
--- Server Version: 5.5.32
--- PHP-Version: 5.4.19
+-- Erstellungszeit: 05. Jul 2018 um 19:07
+-- Server-Version: 10.1.29-MariaDB
+-- PHP-Version: 7.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Datenbank: `pinnwanddb`
 --
-CREATE DATABASE IF NOT EXISTS `pinnwanddb` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `pinnwanddb`;
 
 -- --------------------------------------------------------
 
@@ -28,14 +28,21 @@ USE `pinnwanddb`;
 -- Tabellenstruktur für Tabelle `benutzer`
 --
 
-CREATE TABLE IF NOT EXISTS `benutzer` (
+CREATE TABLE `benutzer` (
   `b_Id` int(11) NOT NULL,
   `b_Vorname` text NOT NULL,
   `b_Nachname` text NOT NULL,
   `b_benutzername` text NOT NULL,
-  `b_password` text NOT NULL,
-  PRIMARY KEY (`b_Id`)
+  `b_password` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Daten für Tabelle `benutzer`
+--
+
+INSERT INTO `benutzer` (`b_Id`, `b_Vorname`, `b_Nachname`, `b_benutzername`, `b_password`) VALUES
+(1, 'Till', 'Schmidt', 'Till33', 'password'),
+(2, 'Georg', 'Popp', 'Popeye', 'DerBoss');
 
 -- --------------------------------------------------------
 
@@ -43,13 +50,37 @@ CREATE TABLE IF NOT EXISTS `benutzer` (
 -- Tabellenstruktur für Tabelle `eintraege`
 --
 
-CREATE TABLE IF NOT EXISTS `eintraege` (
+CREATE TABLE `eintraege` (
   `e_ID` int(11) NOT NULL,
   `e_Text` text NOT NULL,
   `e_Titel` text NOT NULL,
-  `b_Id` int(11) NOT NULL,
-  PRIMARY KEY (`e_ID`)
+  `b_Id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Daten für Tabelle `eintraege`
+--
+
+INSERT INTO `eintraege` (`e_ID`, `e_Text`, `e_Titel`, `b_Id`) VALUES
+(1, 'Fussball ist geil', 'sport', 2),
+(2, 'HTML ist doof', 'Nicht Programmieren', 1);
+
+--
+-- Indizes der exportierten Tabellen
+--
+
+--
+-- Indizes für die Tabelle `benutzer`
+--
+ALTER TABLE `benutzer`
+  ADD PRIMARY KEY (`b_Id`);
+
+--
+-- Indizes für die Tabelle `eintraege`
+--
+ALTER TABLE `eintraege`
+  ADD PRIMARY KEY (`e_ID`);
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
