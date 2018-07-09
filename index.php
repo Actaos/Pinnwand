4b29fb2c59660786860bd98454038fc4a0a768c3
Unknown <Georg.Popp2000@web.de>
2018-07-09 11:41:04 +0200

    New Start-Page
    - Bootstrap
    + New welcome-page
    + new Style.css

--- index.php
+++ index.php
@@ -1,105 +1,63 @@
 <?php
+    // auskommentieren wenn der Funktionsaufruf gestartet werden soll:
+    // require "src/Functions/Export/EntryCSVExport.php";
 
-function sayHello()
-{
-    return "Hello World from PHP!";
-}
+    require "src/Functions/Export/EntryExport.php";
 
+    $skip=0;
+    $top=2;
+    $result= array();
+    $result=get_Entries($skip , $top);
+    // var_dump($result);
 ?>
 
-    <!doctype html>
-    <html lang="en">
+<!DOCTYPE html>
+<html lang="en">
 
-    <head>
-        <!-- Required meta tags -->
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <link rel="stylesheet" href="style.css">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="web/css/style.css">
+    <title>Pinnwand</title>
+</head>
 
-        <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB"
-            crossorigin="anonymous">
-
-        <link href="http://fonts.googleapis.com/css?family=Reenie+Beanie:regular" rel="stylesheet" type="text/css">
-
-        <title>Pinnwand</title>
-    </head>
-
-    <body>
-
-        <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #f4cb42;">
-            <a class="navbar-brand" href="#">
-                <img src="https://cdn.icon-icons.com/icons2/877/PNG/512/inclinated-pinboard_icon-icons.com_68395.png" width="30" height="30"
-                    class="d-inline-block align-top" alt=""> Pinnwand
+<body>
+    <ol>
+        <li>
+            <button href="#">Add Note</button>
+        </li>
+        <li>
+            <button href="#">UAT</button>
+        </li>
+        <li>
+            <button href="#">Export</button>
+        </li>
+        <li>
+            <button href="login.php">Login</button>
+        </li>
+    </ol>
+    <ul>
+    
+    <?php foreach ($result as $key=>$value): ?>
+        <?php if (isset($value['titel'])) { ?>
+        <li>
+            <a href="#">
+                <h2><?= $value['titel']; ?></h2>
+                <p><?= $value['text']; ?></p>
             </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+        </li>
+        <?php }?>
+    <?php endforeach;?>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav my-auto">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="./login.php">Login
-                            <span class="sr-only">(current)</span>
-                        </a>
-                    </li>
-                </ul>
-                <!--<form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>-->
-            </div>
-        </nav>
+    </ul>
 
-        <div class="noteSection">
-            <div class="container">
-                <div class="row">
-                    <ul>
-                        <li>
-                            <a href="#">
-                                <h2>Title #1</h2>
-                                <p>Text Content #1</p>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <h2>Title #2</h2>
-                                <p>Text Content #2</p>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <h2>Hellyeah</h2>
-                                <p>Text Content #3</p>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <h2>Boom!</h2>
-                                <p>Text Content #4</p>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <h2>Last one</h2>
-                                <p>Text Content #5</p>
-                            </a>
-                        </li>
+    <footer>
+        <a href="">All</a> | 
+        <a href="">15</a> | 
+        <a href="">25</a> | 
+        <a href="">50</a> | 
+    </footer>
+</body>
 
-                    </ul>
-                </div>
-            </div>
-        </div>
-
-        <!-- Optional JavaScript -->
-        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-            crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
-            crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T"
-            crossorigin="anonymous"></script>
-    </body>
-
-    </html>
+</html>
